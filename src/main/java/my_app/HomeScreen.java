7d24799c5cdd01777546ab1cd20bce86710aa75b
Eliezer Assunção De Paulo <93846923+eliezer-dev-software-enginner@users.noreply.github.com>
2026-05-13 12:34:51 -0300

    feat: adicionado drag in drop

--- src/main/java/my_app/HomeScreen.java
+++ src/main/java/my_app/HomeScreen.java
@@ -1,10 +1,16 @@
 package my_app;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import megalodonte.ComputedState;
+import megalodonte.ForEachState;
+import megalodonte.ListState;
 import megalodonte.State;
 import megalodonte.base.components.Component;
 import megalodonte.base.components.ScreenComponent;
 import megalodonte.components.Button;
+import megalodonte.components.SpacerHorizontal;
+import megalodonte.components.SpacerVertical;
 import megalodonte.components.Text;
 import megalodonte.components.layout_components.Column;
 import megalodonte.components.layout_components.Container;
@@ -12,6 +18,9 @@ import megalodonte.components.layout_components.Row;
 import megalodonte.props.ColumnProps;
 import megalodonte.props.RowProps;
 import megalodonte.v2.Show;
+
+import java.io.File;
+import java.util.List;
 
 public class HomeScreen implements ScreenComponent {
     FtpServer ftpServer = new FtpServer();
@@ -25,16 +34,62 @@ public class HomeScreen implements ScreenComponent {
     State<Boolean> hasError = State.of(false);
     State<String> errorText = State.of(null);
 
+    ListState<File> filesReceived = ListState.ofEmpty();
+
     @Override
     public Component render() {
+        ForEachState<File, Text> forEachState = ForEachState.of(
+                filesReceived,
+                it -> new Text(it.getName())
+        );
+
         return new Container()
                 .children(
                         new Row(new RowProps().spacingOf(10).paddingAll(20)).children(
-                                new Button(ftpServerText),
+                                new Button(ftpServerText).onClick(()-> runningFtpServer.set(true)),
                                 new Button(ftpClientText).onClick(this::handleRunAsFtpClient)
                         ),
-                        Show.when(hasError, ()-> new Text(errorText))
+                        Show.when(hasError, ()-> new Text(errorText)),
+                        new SpacerVertical(10),
+                        Show.when(runningFtpServer, ()-> dragFileUi()),
+                        new Column(new ColumnProps().centerHorizontally()).items(forEachState)
                 );
+    }
+
+    /*
+    setOnDragOver → permite o drop
+    setOnDragDropped → recebe os arquivos
+     */
+    Component dragFileUi(){
+        Label text = new Label("Arraste arquivos aqui");
+        StackPane layout = new StackPane(text);
+
+        layout.setOnDragOver(event -> {
+            if (event.getGestureSource() != layout
+                    && event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(
+                        javafx.scene.input.TransferMode.COPY
+                );
+            }
+            event.consume();
+        });
+
+        layout.setOnDragDropped(event -> {
+            var dragboard = event.getDragboard();
+            boolean success = false;
+
+            if (dragboard.hasFiles()) {
+                List<File> files = dragboard.getFiles();
+                filesReceived.set(files);
+                success = true;
+            }
+
+            event.setDropCompleted(success);
+            event.consume();
+        });
+
+        return new Container()
+                .children(Component.CreateFromJavaFxNode(layout));
     }
 
     void handleRunAsFtpClient() {
